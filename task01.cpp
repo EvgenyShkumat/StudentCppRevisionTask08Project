60245a80f457f215c53f9ac0113a0b4c253dc7c0
Евгений Шкумат <kotbmasle@gmail.com>
2024-04-27 10:23:37 +0300

    task01 done

--- task01.cpp
+++ task01.cpp
@@ -26,6 +26,17 @@
  */
 
 int task01(int a, int b, int c, int d) {
-	
-	return 0;
+    int result = 0;
+
+    if(a == b && b == c && c == d) {
+        result = 4;
+    }
+    else if((a == b && b == c) || (a == b && b == d) || (a == c && c == d) || (b == c && c == d)) {
+        result = 3;
+    }
+    else if(a == b || a == c || a == d || b == c || b == d || c == d) {
+        result = 2;
+    }
+    
+    return result;
 }
